d718a92f3bf40ffaec1b999cdb331c4722444cb8
liang <123>
2018-10-17 15:12:35 +0800

    04 list.index完成

---
+++ liangliying/src/api/selectList.php
@@ -0,0 +1,40 @@
+<?php
+	
+//header("content-type:text/html;charset=utf-8"); 
+	//查询数据接口
+	
+	//连接数据库
+	include 'connect.php';
+	
+	$page=isset($_GET['page']) ? $_GET['page'] : 1 ;
+	$qty=isset($_GET['qty']) ? $_GET['qty'] : 12 ;
+	//写查询语句
+	$sql='select * from list';
+	
+	//执行查询语句
+	$conn->query("SET NAMES utf8");
+	$res=$conn->query($sql);
+	//获取里面的结果集 (数组类型)
+	$row=$res->fetch_all(MYSQLI_ASSOC);
+//	var_dump($row);
+	//转成字符串
+	
+	
+	//把字符串转成数组
+//	$data = json_decode($row,true);
+//	var_dump($data);
+	$datalist=array(
+		'total' => count($row),
+		'list'  => array_slice($row,($page-1)*$qty,$qty),
+		'page'  => $page,
+//		'qty'   => $qty
+	);
+	
+	//把字符串形式的json数据传给前端  echo
+	echo json_encode($datalist,JSON_UNESCAPED_UNICODE);
+
+
+	//关闭连接数据库
+	$res->close();//关闭结果集
+    $conn->close();//关闭数据库的链接
+?>
--- liangliying/src/js/list.js
+++ liangliying/src/js/list.js
@@ -18,3 +18,99 @@ $('#more').click(function(){
 $('#less').click(function(){
 	$('#more_lists').hide();
 });
+
+//ajax加载数据
+
+function creat(data){
+	var html=``;
+	for(var i=0;i<data.length;i++){
+		html += `
+			<li>
+			<a href=""><img src="../${data[i].url}" alt="" />
+				<span>${data[i].title}</span>
+			</a>
+			<i>${data[i].nowprice}</i>
+			<i>${data[i].price}</i>
+			<p>月销${data[i].count}件</p>
+		</li>
+		`;
+	}
+	$('#productList').html(html);
+}
+
+
+
+$.ajax({
+	type:"get",
+	url:"../api/selectList.php",
+	async:true,
+//	data:{
+//		'page':page,
+//		'qty':12
+//	},
+	success:function(str){
+		//数据渲染
+//		console.log(str);
+		var data = JSON.parse(str);
+//		var arrData = data.list;
+//		console.log(data.list);
+		creat(data.list);
+		
+		//创建分页点
+		//console.log(data.total);
+		var count = Math.ceil(data.total / 12);
+		var html2 = ``;
+		for(var i=0;i<count;i++){
+			html2 += `
+			
+				<li><span class='ipage'>${i+1}</span></li>
+			`;
+		}
+		$('#pages').html('<li><span>上一页</span></li>'+html2 + $('#pages').html());
+		$('#pages span').eq(1).addClass('active');
+		
+		//绑定点击事件，页码跟随切换
+		var nowpage =0;
+		function light(){
+			var span =$("#pages .ipage");
+//			console.log(span.length)
+			$(span).removeClass('active');
+			$('.ipage').eq(nowpage).addClass('active');
+		}
+		
+		$('#pages .ipage').click(function(){
+//			console.log($('#pages .ipage'));
+			//获取到当前点击页码的索引
+			var _this = $('#pages .ipage').index($(this));
+//			console.log(_this);
+			nowpage = _this;
+//			console.log(nowpage);
+			light();
+			
+			//点击哪一页，传页码给接口
+			$.ajax({
+				type:"get",
+				url:"../api/selectList.php",
+				async:true,
+				data:{
+					'page':nowpage,
+					'qty':12
+				},
+				success:function(str){
+					var data = JSON.parse(str);
+					//console.log(data);
+					//分页加载
+					creat(data.list);
+				}
+			});
+			
+			
+			
+		});
+		 
+		
+	}
+});
+
+
+
