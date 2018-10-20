84c455813ae2294f42cc541f791a21271da778c9
liang <123>
2018-10-20 21:56:46 +0800

    02版本

--- liangliying/src/js/reg.js
+++ liangliying/src/js/reg.js
@@ -108,7 +108,7 @@ $('#regBtn').click(function(){
 	var isAgree = $("input[type='checkbox']").is(':checked');
 	//console.log(isAgree);
 	if(isOk && isAgree ){
-//		alert('注册成功！');
+		alert('注册成功！');
 //		console.log(123);
 		window.location.href = "login.html";
 	}else{
